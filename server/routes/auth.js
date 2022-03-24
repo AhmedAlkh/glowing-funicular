b629cdcabde15e005e650cb33a6e01dbe07d97a2
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-03-23 23:03:25 -0400

    6, posting data

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,10 @@
 const express = require('express')
 // To use router from a seperate file//
 const router = express.Router()
+// Require mongoose //
+const mongoose = require('mongoose')
+// User model//
+const User = mongoose.model("User")
 
 // To creare routes//
 router.get('/',(req,res)=>{
@@ -15,7 +19,28 @@ router.post('/signup',(req,res)=>{
     if(!email || !password || !name){
 return res.status(422).json({error:"Information is missing"})
     }
-    res.json({message: "Information was posted"})
+   // To use user model //
+   User.findOne({email:email})
+   .then((savedUser)=>{
+    if(savedUser){
+        return res.status(422).json({error:"User already exists with this email"})    
+    } 
+      const user = new User({
+          email,
+          password,
+          name
+      })
+      user.save()
+      .then(user=>{
+       res.json({message: "Successfully saved"})   
+      })
+      .catch(err=>{
+          console.log(err)
+      })
+   })
+   .catch(err=>{
+    console.log(err) 
+   })
 })
 
 //To export//
