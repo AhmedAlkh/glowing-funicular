a0a773ccf19bde5e14fa230315672ce497a46af6
victoriatsitskishvili <victoria.tsitskishvili@gmail.com>
2022-03-23 21:25:31 -0400

    2, middleware

--- server/app.js
+++ server/app.js
@@ -4,11 +4,25 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 
+//creating middleware//
+const customMiddleware = (req,res,next)=>{
+    console.log("middleware executed!")
+    //to execute the code further//
+    next()
+}
+
 
 //get route//
 app.get('/',(req,res)=>{
+    console.log("home")
 res.send("hello world")
 })
+
+app.get('/about',customMiddleware,(req,res)=>{
+    console.log("about")
+res.send("about page")
+})
+
 
 app.listen(PORT,()=>{
     console.log("server is running on",PORT)
