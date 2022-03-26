d34abe6d60d552b6c0433f8045a950a64d6e99c9
el bandingo <elbandingo@protonmail.com>
2022-03-26 14:39:25 -0230

    added materialize stylesheet to index.html in public. created some more CSS styling for cards and images in profile pages, and home page

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -2,7 +2,20 @@ import React from "react"
 
 const Home = () => {
     return (
-        <h1>Home</h1>
+        <div className="home">
+            <div className="card home-card">
+                <h5>Test User</h5>
+                <div className= "card-image">
+                    <img src="https://images.unsplash.com/photo-1534447677768-be436bb09401?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2694&q=80" />
+                </div>
+                <div className="card-content">
+                <i class="material-icons" style={{color:"red"}}>favorite</i>
+                    <h6>Title</h6>
+                    <p>This is a sample post text</p>
+                    <input type="text" placeholder="add a comment"/>
+                </div>
+            </div>
+        </div>
     );
 }
 
