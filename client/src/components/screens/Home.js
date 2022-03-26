5eac24f9e2fe4aa608bbf5aeaab3015f58f695a0
el bandingo <elbandingo@protonmail.com>
2022-03-26 14:40:48 -0230

    type fix

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
                     <img src="https://images.unsplash.com/photo-1534447677768-be436bb09401?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2694&q=80" />
                 </div>
                 <div className="card-content">
-                <i class="material-icons" style={{color:"red"}}>favorite</i>
+                <i className="material-icons" style={{color:"red"}}>favorite</i>
                     <h6>Title</h6>
                     <p>This is a sample post text</p>
                     <input type="text" placeholder="add a comment"/>
