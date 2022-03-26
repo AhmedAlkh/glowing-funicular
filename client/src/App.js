2dae8d3a0c153af2f6300c194f08b369d064d9e1
el bandingo <elbandingo@protonmail.com>
2022-03-26 21:12:43 -0230

    created context. the old useHistory is now deprecated, and replaced by useNavigate. i will advise the team of these findings for future development and context. Also added state, reducers to apply logic to the homescreen. if you are not signed in, you will not see the homescreen of posts. if you are signed in, the nav bar changes to reflect accurate selection options

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,52 @@
-import React from "react";
+import React,{useEffect, createContext, useReducer, useContext} from "react";
 import NavBar from './components/Navbar';
 import './App.css';
 import { render } from "react-dom";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes, useNavigate} from 'react-router-dom';
 import Home from './components/screens/Home'
 import Signin from './components/screens/Signin'
 import Profile from './components/screens/Profile'
 import Signup from './components/screens/Signup'
 import CreatePost from './components/screens/CreatePost'
+import {reducer, initialState} from './reducers/userReducer'
 
-function App() {
-  return (
-    <Router>
-      <NavBar />
-<Routes>
-  <Route exact path="/" element={<Home />} />
+
+export const UserContext = createContext()
+
+const Routing = ()=>{
+  const navigate = useNavigate()
+  const {state,dispatch} = useContext(UserContext)
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    //console.log(user)
+    if(user){
+      dispatch({type:"USER", payload:user})
+      navigate('/')
+    } else {
+      navigate('/signin')
+    }
+  }, [])
+  return(
+    <Routes>
+      <Route exact path="/" element={<Home />} />
   <Route path="/signin" element={<Signin />} />
   <Route path="/signup" element={<Signup />} />
   <Route path="/profile" element={<Profile />} />
   <Route path="/createpost" element={<CreatePost />} />
-</Routes>
-</Router>
+  </Routes>
+  )
+}
 
+function App() {
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  return (
+    <UserContext.Provider value={{state,dispatch}}>
+    <Router>
+      <NavBar />
+    <Routing />
+    </Router>
+    </UserContext.Provider>
   );
 }
 
--- client/src/components/screens/Signin.js
+++ client/src/components/screens/Signin.js
@@ -1,9 +1,9 @@
-import React, { useState } from "react"
+import React, { useState, useContext,  } from "react"
 import { Link, useNavigate } from "react-router-dom";
 import M from 'materialize-css';
-
+import {UserContext} from '../../App'
 const Login = () => {
-
+    const {state, dispatch} = useContext(UserContext)
     const navigate = useNavigate()
     const [password, setPassword] = useState("")
     const [email, setEmail] = useState("")
@@ -29,6 +29,7 @@ const Login = () => {
             } else {
                 localStorage.setItem("jwt",data.token)
                 localStorage.setItem("user",JSON.stringify(data.user))
+                dispatch({type:"USER",payload: data.user})
                 M.toast({html:"Signed in Successfully", classes:"green"})
                 navigate('/');
             }
