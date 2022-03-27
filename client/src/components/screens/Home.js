a3d7e665698dd86b430a21528bf6445326cb5c54
el bandingo <elbandingo@protonmail.com>
2022-03-26 21:58:41 -0230

    pushing final tested changess. login, signup, create post, view all posts are officially done and mapped

--- client/src/components/screens/Home.js
+++ client/src/components/screens/Home.js
@@ -19,15 +19,15 @@ const Home = () => {
             {
                 data.map(item=>{
                     return(
-                        <div className="card home-card">
-                        <h5>Test User</h5>
+                        <div className="card home-card" key={item._id}>
+                        <h5>{item.postedBy.name}</h5>
                         <div className= "card-image">
-                            <img src="https://images.unsplash.com/photo-1534447677768-be436bb09401?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2694&q=80" />
+                            <img src={item.photo}/>
                         </div>
                         <div className="card-content">
                         <i className="material-icons" style={{color:"red"}}>favorite</i>
-                            <h6>Title</h6>
-                            <p>This is a sample post text</p>
+                            <h6>{item.title}</h6>
+                            <p>{item.body}</p>
                             <input type="text" placeholder="add a comment"/>
                         </div>
                     </div>
