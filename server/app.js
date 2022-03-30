dbe114b31bbb3b058d5fea3d6f1f5af31ccdc0be
AhmedA <alkhatib_ahmed1995@hotmail.com>
2022-03-29 21:02:48 -0400

    updated app js file, preparing to deploy to heroku

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-const PORT = 3001;
-const {MONGOURI} = require('./keys');
+const PORT = process.env.PORT || 3001;
+const {MONGOURI} = require('./config/keys');
 
 //require the user Models
 require('./models/user');
@@ -34,6 +34,13 @@ app.use(require('./routes/auth'))
 app.use(require('./routes/post'))
 app.use(require('./routes/user'))
 
+if(process.env.NODE_ENV=='production'){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
